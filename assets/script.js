962c90eb9263d29ea53d68164650e41560b927e6
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-04-14 05:42:43 -0300

    crud cadastrar

--- assets/script.js
+++ assets/script.js
@@ -59,14 +59,60 @@ const findPaletaById = async () => {
     }
 };
 
+function fecharModal() {
+  document.querySelector(".modal-overlay1").style.display = "none";
+}
+
 function abrirModalCadastro() {
   document.querySelector(".modal-overlay2").style.display = "flex";
 }
 
 function fecharModalCadastro() {
   document.querySelector(".modal-overlay2").style.display = "none";
+  document.querySelector("#sabor").value = "";
+  document.querySelector("#preco").value = 0;
+  document.querySelector("#descricao").value = "";
+  document.querySelector("#foto").value = "";
 }
 
-function fecharModal() {
-  document.querySelector(".modal-overlay1").style.display = "none";
+async function createPaleta() {
+  const sabor = document.querySelector("#sabor").value;
+  const preco = document.querySelector("#preco").value;
+  const descricao = document.querySelector("#descricao").value;
+  const foto = document.querySelector("#foto").value;
+
+  const paleta = {
+    sabor,
+    preco,
+    descricao,
+    foto,
+  };
+
+  const response = await fetch(`${baseUrl}/create`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    mode: "cors",
+    body: JSON.stringify(paleta),
+  });
+
+  const novaPaleta = await response.json();
+
+  const html = `
+  <div class="paletaItem">
+    <div>
+        <div class="paletaItem__sabor">${novaPaleta.sabor}</div>
+        <div class="paletaItem__preco">R$ ${novaPaleta.preco}</div>
+        <div class="paletaItem__descricao">${novaPaleta.descricao}</div>
+    </div>
+    <img class="paletaItem__img" src="${novaPaleta.foto}" alt="Paleta de ${novaPaleta.sabor}" />
+  </div>`;
+
+  document.querySelector("#contentList").insertAdjacentHTML("beforeend", html);
+
+  fecharModalCadastro();
 }
+
+
+
