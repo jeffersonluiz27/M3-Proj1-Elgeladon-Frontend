d966514e8bb37b1366988770eab0751418592535
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-04-13 22:00:44 -0300

    busca modal

--- assets/script.js
+++ assets/script.js
@@ -27,3 +27,42 @@ async function findAllPaletas() {
 };
 
 findAllPaletas();
+
+const findPaletaById = async () => {
+
+  const id = document.getElementById("idPaleta").value;
+
+  if(id <= 0){
+    alert('Digite um numero maior que 0!');
+  }else{
+
+      document.querySelector(".modal-overlay").style.display = "flex";
+
+      const response = await fetch(`${baseUrl}/find-paleta/${id}`);
+
+      const paleta = await response.json();
+
+      const paletaEscolhidaDiv = document.getElementById("paletaEscolhida");
+
+      paletaEscolhidaDiv.innerHTML = `<div class="paletaItem">
+      <div>
+          <div class="paletaItem__sabor">${paleta.sabor}</div>
+          <div class="paletaItem__preco">R$ ${paleta.preco.toFixed(
+            2
+          )}</div>
+          <div class="paletaItem__descricao">${paleta.descricao}</div>
+      </div>
+          <img class="paletaItem__img" src=${
+            paleta.foto
+          } alt=${`Paleta de ${paleta.sabor}`} />
+      </div>`;
+    }
+};
+
+function abrirModalCadastro() {
+  document.querySelector(".modal-overlay").style.display = "flex";
+}
+
+function fecharModal() {
+  document.querySelector(".modal-overlay").style.display = "none";
+}
